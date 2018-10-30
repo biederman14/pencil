758d9040160f500adb2c5146b9c8ae499e65694c
Ingrid Biederman <biederman.14@gmail.com>
2018-10-29 20:20:06 -0400

    Added: pencil printing empty spaces, writingAdd now private, point durability, writing.length, substring, and check for substring

--- src/main/java/org/wecancodeit/pencil/Pencil.java
+++ src/main/java/org/wecancodeit/pencil/Pencil.java
@@ -2,10 +2,27 @@ package org.wecancodeit.pencil;
 
 public class Pencil {
 
-	String writingAdd = "";
+	private String writingAdd = "";
+	private int pointDurability;
+
+	public Pencil(int startingDurability) {
+		pointDurability = startingDurability;
+	}
 
 	public void write(String writing) {
-		writingAdd += writing;
+		// substring is counting every character up until it hits the pointDurability
+		// number and then stops
+		if (writing.length() > pointDurability) {
+			String subWriting = writing.substring(0, pointDurability);
+			writingAdd += subWriting;
+			// this creates the blank spaces and adds it to the end of writingAdd
+			int blankSpaces = writing.length() - pointDurability;
+			for (int padding = 0; padding < blankSpaces; padding++) {
+				writingAdd += " ";
+			}
+		} else {
+			writingAdd += writing;
+		}
 	}
 
 	public String written() {
--- src/test/java/org/wecancodeit/pencil/PencilTest.java
+++ src/test/java/org/wecancodeit/pencil/PencilTest.java
@@ -11,7 +11,7 @@ public class PencilTest {
 
 	@Before
 	public void setUp() {
-		subject = new Pencil();
+		subject = new Pencil(50);
 	}
 
 	@Test
@@ -26,5 +26,12 @@ public class PencilTest {
 		subject.write("Hello, World.");
 		subject.write("Han shot first!");
 		assertEquals("Hello, World.Han shot first!", subject.written());
+	}
+
+	@Test
+	public void pencilGoesDull() {
+		subject = new Pencil(10);
+		subject.write("seedpuppies?");
+		assertEquals("seedpuppie  ", subject.written());
 	}
 }
